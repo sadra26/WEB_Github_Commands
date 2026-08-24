6063dd596d68155dda9291bac13a35a35ca8bee3
sadra26 <alzastro12345@gmail.com>
2026-08-25 00:22:22 +0700

    Add login feature

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("Hello world")
+
+function login(user) {
+    if (user === "admin") {
+        console.log("Succeed to login")
+    } else {
+        console.log("Failed to login")
+    }
+
+    return;
+}
+
+login("admi")
